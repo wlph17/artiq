47d9afcdb039f5b89c60f6e48eb2edf4737c90a5
wlph17 <wl@m-labs.hk>
2022-12-22 17:27:02 +0800

    debug:artiq_master --version

--- artiq/_version.py
+++ artiq/_version.py
@@ -2,7 +2,11 @@ import os
 import subprocess
 
 def get_version():
-    return os.getenv("VERSIONEER_OVERRIDE", default="8.0.beta")
+    return os.getenv("VERSIONEER_OVERRIDE", default="8" + "." + \
+     subprocess.check_output(['git', 'rev-list', '--count', 'HEAD']).decode('ascii').strip() + "." + \
+     subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('ascii').strip() + "." + \
+     "beta")
 
 def get_rev():
-    return os.getenv("VERSIONEER_REV", default=subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('ascii').strip())
+    return os.getenv("VERSIONEER_REV", default= \
+     subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('ascii').strip())
