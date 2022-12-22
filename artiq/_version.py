dcd2bafd788568e41e84a0f6c54e9fb77028fe74
wlph17 <wl@m-labs.hk>
2022-12-22 15:11:42 +0800

    debug:afws_client cannot find git hash

--- artiq/_version.py
+++ artiq/_version.py
@@ -1,4 +1,8 @@
 import os
+import subprocess
 
 def get_version():
     return os.getenv("VERSIONEER_OVERRIDE", default="8.0.beta")
+
+def get_rev():
+    return os.getenv("VERSIONEER_REV", default=subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('ascii').strip())
